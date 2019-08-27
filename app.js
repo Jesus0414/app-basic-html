3990123bda776d90aea99f9a30d4ee1952aea2e5
Jesus_0414 <jesus140400@gmail.com>
2019-08-27 10:17:30 -0700

    app.js suena

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 
 const buttons = document.querySelectorAll('button');
 
-const playnote = event =>{
+let playNote = event => {
+    console.log(event.target)
+    const button = event.target;
+    const note = button.dataset.note;
+    console.log(`audio${note}`);
+    const audio = document.getElementById(`audio${note}`);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+}
+ 
+buttons.forEach(
+    button => button.addEventListener('click', playNote)
+);
+
+
+/*const playnote = event =>{
     console.log(event.target.dataset.note);
 }
 
 buttons.forEach(
     button => button.addEventListener('click', playnote)
-)
+)*/
